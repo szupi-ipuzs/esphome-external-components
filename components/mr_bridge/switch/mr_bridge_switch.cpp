c72239ecb118cbe990c93132649b9a3b6ee49f6e
Piotr Szulc <piotr.szulc@gmail.com>
2024-05-28 01:43:35 +0200

    Fix for initial state

--- components/mr_bridge/switch/mr_bridge_switch.cpp
+++ components/mr_bridge/switch/mr_bridge_switch.cpp
@@ -12,13 +12,14 @@ void MRBridgeSwitch::set_mrbridge_config(InternalGPIOPin *pin_fwd, InternalGPIOP
    m_pin_fwd = pin_fwd;
    m_pin_rev = pin_rev;
    m_pulse_len = pulse_len;
+   m_pulse_cnt = 0;
 }
 
 void MRBridgeSwitch::setup()
 {
    this->m_pin_fwd->setup();
-   this->m_pin_fwd->digital_write(false);
    this->m_pin_rev->setup();
+   this->m_pin_fwd->digital_write(false);
    this->m_pin_rev->digital_write(false);
 
    bool initial_state = this->get_initial_state_with_restore_mode().value_or(false);
@@ -32,6 +33,7 @@ void MRBridgeSwitch::setup()
 
 void MRBridgeSwitch::write_state(bool state)
 {
+   ESP_LOGCONFIG(TAG, "Requesting new state: %d", state);
    m_new_state = state;
 }
 
@@ -56,20 +58,22 @@ void MRBridgeSwitch::loop()
       }
    }
    else
-   if (m_current_state < m_new_state)
+   if (m_current_state != m_new_state)
    {
-      ESP_LOGCONFIG(TAG, "Forward pulse initiated");
-      this->m_pin_fwd->digital_write(true);
-      m_current_state = m_new_state;
-      m_pulse_cnt = m_pulse_len;
-   }
-   else
-   if (m_current_state > m_new_state)
-   {
-      ESP_LOGCONFIG(TAG, "Reverse pulse initiated");
-      this->m_pin_rev->digital_write(true);
-      m_current_state = m_new_state;
-      m_pulse_cnt = m_pulse_len;
+      if (m_new_state)
+      {
+         ESP_LOGCONFIG(TAG, "Forward pulse initiated");
+         this->m_pin_fwd->digital_write(true);
+         m_current_state = m_new_state;
+         m_pulse_cnt = m_pulse_len;
+      }
+      else
+      {
+         ESP_LOGCONFIG(TAG, "Reverse pulse initiated");
+         this->m_pin_rev->digital_write(true);
+         m_current_state = m_new_state;
+         m_pulse_cnt = m_pulse_len;
+      }
    }
 }
 
