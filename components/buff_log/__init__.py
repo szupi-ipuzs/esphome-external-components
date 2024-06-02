3c048541ed1b3f96819450d2ef8bd3850f76c1a1
Piotr Szulc <piotr.szulc@gmail.com>
2024-06-02 15:29:22 +0200

    BufferingLogger: added possibility to trigger dumping via timeout. Added dump finished lambda.

--- components/buff_log/__init__.py
+++ components/buff_log/__init__.py
@@ -1,5 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome import core
 from esphome.const import (
     CONF_ID
 )
@@ -9,26 +10,37 @@ DEPENDENCIES = ["logger"]
 CODEOWNERS = ["@szupi-ipuzs"]
 buff_log_ns = cg.esphome_ns.namespace("buff_log")
 
+CONF_BUFFERING_TIMEOUT = "buffering_timeout"
 CONF_SIZE_LIMIT = "size_limit"
 CONF_DUMP_LINES_PER_LOOP = "dump_lines_per_loop"
 CONF_ON_DUMP_LINE = "on_dump_line"
+CONF_ON_DUMP_FINISHED = "on_dump_finished"
 
 BufferingLogger = buff_log_ns.class_('BufferingLogger', cg.Component)
 
 CONFIG_SCHEMA = cv.All(
    cv.Schema({
       cv.GenerateID(CONF_ID): cv.declare_id(BufferingLogger),
+      cv.Optional(CONF_BUFFERING_TIMEOUT, default="0ms"): cv.All(
+                cv.positive_time_period_milliseconds,
+                cv.Range(min=core.TimePeriod(milliseconds=0), max=core.TimePeriod(milliseconds=10000)),
+            ),
       cv.Required(CONF_SIZE_LIMIT): cv.int_range(min=1024, max=1048576),
       cv.Optional(CONF_DUMP_LINES_PER_LOOP, default=20): cv.int_range(min=5, max=100),
-      cv.Required(CONF_ON_DUMP_LINE): cv.lambda_
+      cv.Required(CONF_ON_DUMP_LINE): cv.lambda_,
+      cv.Optional(CONF_ON_DUMP_FINISHED): cv.lambda_
    })
 )
 
 async def to_code(config):
-   lambda_ = await cg.process_lambda(
+   dump_line_lambda_ = await cg.process_lambda(
          config[CONF_ON_DUMP_LINE],
          [(buff_log_ns.LogItem, "item")],
          return_type=cg.void,
    )
-   var = cg.new_Pvariable(config[CONF_ID], config[CONF_SIZE_LIMIT], config[CONF_DUMP_LINES_PER_LOOP], lambda_)
+   if CONF_ON_DUMP_FINISHED in config:
+      dump_finished_lambda_ = await cg.process_lambda(config[CONF_ON_DUMP_FINISHED], [(buff_log_ns.DumpingTrigger, "trigger")], return_type=cg.void)
+      var = cg.new_Pvariable(config[CONF_ID], config[CONF_BUFFERING_TIMEOUT], config[CONF_SIZE_LIMIT], config[CONF_DUMP_LINES_PER_LOOP], dump_line_lambda_, dump_finished_lambda_)
+   else:
+      var = cg.new_Pvariable(config[CONF_ID], config[CONF_BUFFERING_TIMEOUT], config[CONF_SIZE_LIMIT], config[CONF_DUMP_LINES_PER_LOOP], dump_line_lambda_)
    await cg.register_component(var, config)
--- components/buff_log/buff_log.h
+++ components/buff_log/buff_log.h
@@ -22,14 +22,24 @@ struct LogItem
 
 static const char* TAG = "bufflog";
 
+enum DumpingTrigger
+{
+   NONE,
+   OVERFLOW,
+   TIMEOUT,
+   FORCED
+};
+
 class BufferingLogger : public Component
 {
 public:
 
-   BufferingLogger(std::uint32_t sizeLimit, std::uint32_t dumpLinesPerLoop, std::function<void(const LogItem&)> dumpProcessor):
+   BufferingLogger(std::uint32_t bufferingTimeout, std::uint32_t sizeLimit, std::uint32_t dumpLinesPerLoop, std::function<void(const LogItem&)> dumpProcessor, std::function<void(DumpingTrigger)> dumpFinishedHandler = [](DumpingTrigger){}):
+   m_bufferingTimeout(bufferingTimeout),
    m_sizeLimit(sizeLimit),
    m_dumpLinesPerLoop(dumpLinesPerLoop),
-   m_dumpProcessor(std::move(dumpProcessor))
+   m_dumpProcessor(std::move(dumpProcessor)),
+   m_dumpFinishedHandler(std::move(dumpFinishedHandler))
    {}
 
    float get_setup_priority() const override
@@ -62,31 +72,26 @@ public:
             if (m_currentSize >= m_sizeLimit)
             {
                m_state = State::DUMPING;
+               m_trigger = DumpingTrigger::OVERFLOW;
+               ESP_LOGI(TAG, "Starting dump due to overflow");
             }
          });
       }
 
       m_state = State::BUFFERING;
+      m_startTimestamp = esphome::millis();
    }
 
    void loop() override
    {
-      uint32_t itemsDumped = 0;
-      while ((m_state == State::DUMPING) && (!m_paused) && (itemsDumped < m_dumpLinesPerLoop))
+      if (m_state == State::BUFFERING)
       {
-         if (m_items.empty())
-         {
-            stop();
-            break;
-         }
+         loopWhenBuffering();
+      }
 
-         ESP_LOGI(TAG, "dumping, left=%zu", m_items.size());
-
-         auto item = std::move(m_items.front());
-         m_items.pop();
-         m_currentSize -= item.combinedLength;
-         m_dumpProcessor(item);
-         ++itemsDumped;
+      if (m_state == State::DUMPING)
+      {
+         loopWhenDumping();
       }
    }
 
@@ -103,6 +108,8 @@ public:
       clearQueue();
       m_paused = false;
       m_state = State::BUFFERING;
+      m_startTimestamp = esphome::millis();
+      m_trigger = DumpingTrigger::NONE;
    }
 
    bool isBuffering() const
@@ -119,6 +126,8 @@ public:
    {
       m_state = State::DUMPING;
       m_paused = false;
+      m_trigger = DumpingTrigger::FORCED;
+      ESP_LOGI(TAG, "Forcing dump");
    }
 
    void pause()
@@ -131,6 +140,11 @@ public:
    {
       m_paused = false;
       ESP_LOGI(TAG, "Resuming");
+   }
+
+   DumpingTrigger getDumpingTrigger() const
+   {
+      return m_trigger;
    }
 
 private:
@@ -149,11 +163,48 @@ private:
       m_currentSize = 0;
    }
 
+   void loopWhenBuffering()
+   {
+      if ((m_bufferingTimeout != 0) && (m_startTimestamp - millis() > m_bufferingTimeout))
+      {
+         m_paused = false;
+         m_state = State::DUMPING;
+         m_trigger = DumpingTrigger::TIMEOUT;
+         ESP_LOGI(TAG, "Starting dump due to timeout");
+      }
+   }
+
+   void loopWhenDumping()
+   {
+      uint32_t itemsDumped = 0;
+      while ((m_state == State::DUMPING) && (!m_paused) && (itemsDumped < m_dumpLinesPerLoop))
+      {
+         if (m_items.empty())
+         {
+            stop();
+            m_dumpFinishedHandler(m_trigger);
+            break;
+         }
+
+         ESP_LOGI(TAG, "dumping, left=%zu", m_items.size());
+
+         auto item = std::move(m_items.front());
+         m_items.pop();
+         m_currentSize -= item.combinedLength;
+         m_dumpProcessor(item);
+         ++itemsDumped;
+      }
+   }
+
+   const std::uint32_t m_bufferingTimeout;
    const std::uint32_t m_sizeLimit;
    const std::uint32_t m_dumpLinesPerLoop;
    std::function<void(const LogItem&)> m_dumpProcessor;
+   std::function<void(DumpingTrigger)> m_dumpFinishedHandler;
    State m_state{State::IDLE};
+   std::uint32_t m_startTimestamp{0u};
    bool m_paused{false};
+   DumpingTrigger m_trigger{DumpingTrigger::NONE};
 
    std::queue<LogItem> m_items;
    std::uint32_t m_currentSize{0u};
