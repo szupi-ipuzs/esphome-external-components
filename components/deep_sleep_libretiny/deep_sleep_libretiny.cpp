54446a1492492c1e637c43f84a5946615a970ee2
Piotr Szulc <piotr.szulc@gmail.com>
2024-07-28 21:54:05 +0200

    Fixed invert wakeup

--- components/deep_sleep_libretiny/deep_sleep_libretiny.cpp
+++ components/deep_sleep_libretiny/deep_sleep_libretiny.cpp
@@ -1,11 +1,12 @@
 #include "deep_sleep_libretiny.h"
-#include <cinttypes>
 #include "esphome/core/application.h"
 #include "esphome/core/log.h"
+#include <cinttypes>
 
 namespace esphome {
 namespace deep_sleep {
-bool global_has_deep_sleep = false;  // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
+bool global_has_deep_sleep =
+    false; // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
 }
 
 namespace deep_sleep_libretiny {
@@ -22,10 +23,12 @@ void DeepSleepLibretiny::setup() {
 
   const optional<uint32_t> run_duration = get_run_duration_();
   if (run_duration.has_value()) {
-    ESP_LOGI(TAG, "Scheduling Deep Sleep to start in %" PRIu32 " ms", *run_duration);
+    ESP_LOGI(TAG, "Scheduling Deep Sleep to start in %" PRIu32 " ms",
+             *run_duration);
     this->set_timeout(*run_duration, [this]() { this->begin_sleep(); });
   } else {
-    ESP_LOGD(TAG, "Not scheduling Deep Sleep, as no run duration is configured.");
+    ESP_LOGD(TAG,
+             "Not scheduling Deep Sleep, as no run duration is configured.");
   }
 }
 void DeepSleepLibretiny::dump_config() {
@@ -40,8 +43,10 @@ void DeepSleepLibretiny::dump_config() {
   if (this->wakeup_cause_to_run_duration_.has_value()) {
     ESP_LOGCONFIG(TAG, "  Default Wakeup Run Duration: %" PRIu32 " ms",
                   this->wakeup_cause_to_run_duration_->default_cause);
-    ESP_LOGCONFIG(TAG, "  Touch Wakeup Run Duration: %" PRIu32 " ms", this->wakeup_cause_to_run_duration_->touch_cause);
-    ESP_LOGCONFIG(TAG, "  GPIO Wakeup Run Duration: %" PRIu32 " ms", this->wakeup_cause_to_run_duration_->gpio_cause);
+    ESP_LOGCONFIG(TAG, "  Touch Wakeup Run Duration: %" PRIu32 " ms",
+                  this->wakeup_cause_to_run_duration_->touch_cause);
+    ESP_LOGCONFIG(TAG, "  GPIO Wakeup Run Duration: %" PRIu32 " ms",
+                  this->wakeup_cause_to_run_duration_->gpio_cause);
   }
   if (wakeup_pins_.size() > 0) {
     for (WakeUpPinItem item : this->wakeup_pins_) {
@@ -54,37 +59,42 @@ void DeepSleepLibretiny::loop() {
     this->begin_sleep();
 }
 float DeepSleepLibretiny::get_loop_priority() const {
-  return -100.0f;  // run after everything else is ready
+  return -100.0f; // run after everything else is ready
 }
-void DeepSleepLibretiny::set_sleep_duration(uint32_t time_ms) { this->sleep_duration_ = uint64_t(time_ms) * 1000; }
+void DeepSleepLibretiny::set_sleep_duration(uint32_t time_ms) {
+  this->sleep_duration_ = uint64_t(time_ms) * 1000;
+}
 
-void DeepSleepLibretiny::set_run_duration(WakeupCauseToRunDuration wakeup_cause_to_run_duration) {
+void DeepSleepLibretiny::set_run_duration(
+    WakeupCauseToRunDuration wakeup_cause_to_run_duration) {
   wakeup_cause_to_run_duration_ = wakeup_cause_to_run_duration;
 }
 
-bool DeepSleepLibretiny::pin_prevents_sleep(WakeUpPinItem& pinItem) {
-  return (pinItem.wakeup_pin_mode == WAKEUP_PIN_MODE_KEEP_AWAKE && pinItem.wakeup_pin != nullptr &&
-          !this->sleep_duration_.has_value() && (pinItem.wakeup_level == pinItem.wakeup_pin->digital_read()));
+bool DeepSleepLibretiny::pin_prevents_sleep(WakeUpPinItem &pinItem) {
+  return (pinItem.wakeup_pin_mode == WAKEUP_PIN_MODE_KEEP_AWAKE &&
+          pinItem.wakeup_pin != nullptr && !this->sleep_duration_.has_value() &&
+          (pinItem.wakeup_level == pinItem.wakeup_pin->digital_read()));
 }
 
-void DeepSleepLibretiny::set_run_duration(uint32_t time_ms) { this->run_duration_ = time_ms; }
+void DeepSleepLibretiny::set_run_duration(uint32_t time_ms) {
+  this->run_duration_ = time_ms;
+}
 void DeepSleepLibretiny::begin_sleep(bool manual) {
   if (this->prevent_ && !manual) {
     this->next_enter_deep_sleep_ = true;
     return;
   }
   if (wakeup_pins_.size() > 0) {
-    for (WakeUpPinItem& item : this->wakeup_pins_)
-    {
-      if (pin_prevents_sleep(item))
-      {
-          // Defer deep sleep until inactive
-          if (!this->next_enter_deep_sleep_) {
-            this->status_set_warning();
-          }
-          ESP_LOGW(TAG, "Waiting for pin_ to switch state to enter deep sleep...");
-          this->next_enter_deep_sleep_ = true;
-          return;
+    for (WakeUpPinItem &item : this->wakeup_pins_) {
+      if (pin_prevents_sleep(item)) {
+        // Defer deep sleep until inactive
+        if (!this->next_enter_deep_sleep_) {
+          this->status_set_warning();
+        }
+        ESP_LOGW(TAG,
+                 "Waiting for pin_ to switch state to enter deep sleep...");
+        this->next_enter_deep_sleep_ = true;
+        return;
       }
     }
   }
@@ -98,18 +108,23 @@ void DeepSleepLibretiny::begin_sleep(bool manual) {
   if (this->sleep_duration_.has_value())
     lt_deep_sleep_config_timer((*this->sleep_duration_ / 1000) & 0xFFFFFFFF);
   if (wakeup_pins_.size() > 0) {
-    for (WakeUpPinItem& item : this->wakeup_pins_) {
-      if (item.wakeup_pin_mode == WAKEUP_PIN_MODE_INVERT_WAKEUP && item.wakeup_pin->digital_read()) {
-        item.wakeup_level = !item.wakeup_pin->digital_read();
+    for (WakeUpPinItem &item : this->wakeup_pins_) {
+      if (item.wakeup_pin_mode == WAKEUP_PIN_MODE_INVERT_WAKEUP) {
+        if (item.wakeup_level == item.wakeup_pin->digital_read()) {
+          item.wakeup_level = !item.wakeup_level;
+        }
       }
-      lt_deep_sleep_config_gpio(1 << item.wakeup_pin->get_pin(), item.wakeup_level);
+      lt_deep_sleep_config_gpio(1 << item.wakeup_pin->get_pin(),
+                                item.wakeup_level);
     }
   }
   lt_deep_sleep_enter();
 }
-float DeepSleepLibretiny::get_setup_priority() const { return setup_priority::DATA; }
+float DeepSleepLibretiny::get_setup_priority() const {
+  return setup_priority::DATA;
+}
 void DeepSleepLibretiny::prevent_deep_sleep() { this->prevent_ = true; }
 void DeepSleepLibretiny::allow_deep_sleep() { this->prevent_ = false; }
 
-}  // namespace deep_sleep_libretiny
-}  // namespace esphome
+} // namespace deep_sleep_libretiny
+} // namespace esphome
